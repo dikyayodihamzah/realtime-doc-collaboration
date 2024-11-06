87bebd5b0f9386ad25d5b7a98f385356578c5087
dikyayodihamzah <dikyafamby@synapsis.id>
2024-11-06 13:50:16 +0700

    chore: add websocket server

--- app/main.go
+++ app/main.go
@@ -1,34 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/dikyayodihamzah/realtime-doc-collaboration/app/service/hubsrv"
+	"github.com/dikyayodihamzah/realtime-doc-collaboration/app/service/wssrv"
 	"github.com/dikyayodihamzah/realtime-doc-collaboration/pkg/config"
 	"github.com/dikyayodihamzah/realtime-doc-collaboration/pkg/model"
+
+	_ "github.com/joho/godotenv/autoload"
 )
-
-// serveWebSocket upgrades the HTTP connection to WebSocket and registers it
-func serveWebSocket(h *model.Hub, w http.ResponseWriter, r *http.Request) {
-	upgrader := config.NewUpgrader()
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("WebSocket upgrade failed:", err)
-		return
-	}
-
-	client := &model.Connection{
-		WS:   conn,
-		Send: make(chan []byte, 256),
-	}
-	h.Register <- client
-
-	// Start write and read pumps for the connection
-	go client.WritePump()
-	go client.ReadPump(h)
-}
 
 // Main function to set up the server
 func main() {
@@ -36,11 +19,15 @@ func main() {
 	hub := config.NewHub(doc)
 	go hubsrv.Run(hub)
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWebSocket(hub, w, r)
+	wss := wssrv.New(hub)
+	wsTopic := os.Getenv("WS_TOPIC")
+	// wsTopic := "/ws"
+	http.HandleFunc(wsTopic, func(w http.ResponseWriter, r *http.Request) {
+		wss.Serve(w, r)
 	})
 
-	addr := ":8080"
-	fmt.Println("Starting server on", addr)
+	addr := os.Getenv("SERVER_PORT")
+	// addr := ":8080"
+	log.Println("Starting server on", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
